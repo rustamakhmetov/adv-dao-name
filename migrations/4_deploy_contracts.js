a34efb1cc59a9f0836abd5c772a51e14f4afb28d
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-04-09 13:59:40 +0500

    remove the vote for the symbol name

--- migrations/4_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var DAOSymbolContract = artifacts.require("DAOSymbolContract");
-var DAOToken = artifacts.require("DAOToken");
-
-module.exports = function(deployer) {
-    deployer.deploy(DAOSymbolContract, DAOToken.address)
-};
